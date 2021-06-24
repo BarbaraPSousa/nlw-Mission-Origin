d295d11ef111b2c4f1b227a44f17331ef31a565a
BarbaraPSousa <barbarapsousa@hotmail.com>
2021-06-24 09:55:39 -0300

    [Mission: Origin] Aula 02/02 – Expandindo o conhecimento

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 //DOM Document Object Model
 
-/*função de fechar o mennu ao clicar*/
+/*função de fechar o menu ao clicar*/
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
+  //se tiver a class show tira, se não coloca
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
@@ -13,8 +14,21 @@ for (const element of toggle) {
 /*função de clicar em um item do menu, esconder o menu*/
 const links = document.querySelectorAll('nav ul li a')
 
-for (const links of links) {
-  links.addEventListener('click', function () {
+for (const link of links) {
+  link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
+/* mudar o header da pagina quando der scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    /* maior que a altura do header*/
+    header.classList.add('scroll') //adiciona
+  } else {
+    /* menor que a altura do header*/
+    header.classList.remove('scroll') //remove
+  }
+})
