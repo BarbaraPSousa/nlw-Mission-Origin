b3b86bc11d85a0d3284120a73b6d4ae6e83c0702
BarbaraPSousa <barbarapsousa@hotmail.com>
2021-06-26 09:24:53 -0300

    [Mission: Origin] Aula 05 – Finalizando o projeto

--- main.js
+++ main.js
@@ -12,18 +12,19 @@ for (const element of toggle) {
 }
 
 /*função de clicar em um item do menu, esconder o menu*/
-const links = document.querySelectorAll('nav ul li a')
 
+const links = document.querySelectorAll('nav ul li a')
 for (const link of links) {
   link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
-/* mudar o header da pagina quando der scroll*/
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
 
+/* mudar o header da pagina quando der scroll*/
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     /* maior que a altura do header*/
     header.classList.add('scroll') //adiciona
@@ -34,15 +35,24 @@ function changeHeaderWhenScroll() {
 }
 
 /* Testimonials carousel slider swiper*/
+
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
+
 /*ScrollReveal: mostra o elemento quando da scroll na pagina*/
+
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
@@ -63,9 +73,8 @@ scrollReveal.reveal(
 
 /*Botom volta para o topo*/
 
+const backToTopButton = document.querySelector('.back-to-top')
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
-
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -73,8 +82,35 @@ function backToTop() {
   }
 }
 
+/*Memu ativo conforme a seção visivel na página*/
+
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeigt = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeigt
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul il a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul il a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 /*When Scroll*/
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection
 })
